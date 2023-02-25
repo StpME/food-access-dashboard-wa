33ae755dff07c8d57cabd61bda7c19dd7adb9bdf
Mitchell S <m.e.stapelman@gmail.com>
2023-02-24 23:48:28 -0800

    add about html page, link to about in nav

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,12 @@
-//temp navbar
+
 const navbar = `
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Food Deserts in Washington</a>
            
-        
-            </div>
+            <a class="btn btn-light me-2" href="about_info.html" role="button">About</a>
+            
+            
         </div>
     </nav>
     `
