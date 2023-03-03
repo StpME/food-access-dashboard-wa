53aa280dfd08f06ff531122a142c8ccdfb0c41da
Kyle <leungkyle2@gmail.com>
2023-03-03 01:56:04 -0800

    page layout

--- scripts/mapbox.js
+++ scripts/mapbox.js
@@ -9,42 +9,110 @@
 
 
 mapboxgl.accessToken =
-    'pk.eyJ1IjoibWl0Y2hlbGxzIiwiYSI6ImNsZWRlaDcwMTAzNDgzcnBjbnFwd24yNzEifQ.ObmlR1g0krQvk3b0S0V3qQ';
+            'pk.eyJ1IjoiZGltZW50aW8iLCJhIjoiY2xhMngzZmEyMDRtdDN2bW93MjYyY2hvbSJ9.lBP2u-C8BEgug7_ye16y2g';
 let map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/dark-v10',
-    zoom: 6.7, // starting zoom - washington state level
-    center: [-120.79438630057388, 47.53695842091479], // starting center - washington state
+    style: 'mapbox://styles/mapbox/dark-v11', // style URL
+    center: [-120.79438630057388, 47.53695842091479], // starting position [lng, lat]
+    zoom: 6.5, // starting zoom
     projection: 'mercator'
 });
 // Full Screen toggle
 map.addControl(new mapboxgl.FullscreenControl(), 'bottom-right');
 // Zoom/nav control
 map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
-// Load Map
 map.on('load', () => {
-
-    // Add Source
-    
-    /*map.addSource('WA-base', {
-        'type': 'raster',
-        'tiles': [
-            'assets/wa-basemap/{z}/{x}/{y}.png'
-        ],
-        'tileSize': 256,
+    map.addSource('median', {
+        type: 'geojson',
+        data: 'assets/WA_QGIS.geojson'
     });
 
-    // Add Layer
+    map.addLayer({
+        'id': 'median_income',
+        'type': 'circle',
+        'source': 'median',
+        'paint': {
+            // increase the radii of the circle (not working)
+            'circle-radius': {
+                'property': 'MedianFamilyIncome',
+                'stops': [
+                    [grades[0], radii[0]],
+                    [grades[1], radii[1]],
+                    [grades[2], radii[2]]
+                ]
+            },
+            'circle-color': {
+                'property': 'MedianFamilyIncome',
+                'stops': [
+                    [grades[0], colors[0]],
+                    [grades[1], colors[1]],
+                    [grades[2], colors[2]]
+                ]
+            },
+            'circle-stroke-color': 'white',
+            'circle-stroke-width': 1,
+            'circle-opacity': 0.6
+
+
+
+            
+        }
+    });
+
+    map.addSource('maine', {
+        'type': 'geojson',
+        'data': 'assets/WA_QGIS.geojson'
+        });
 
     map.addLayer({
-        'id': 'WA basemap',
-        'type': 'raster',
-        'layout': {
-            'visibility': 'none'
-        },
-        'source': 'king-base'
-    }); */
+        'id': 'maine',
+        'type': 'fill',
+        'source': 'maine', // reference the data source
+        'layout': {},
+        'paint': {
+        'fill-color': '#0080ff', // blue color fill
+        'fill-opacity': 0.5
+        }
+        });
+
 });
+
+const grades = [50000, 75000, 100000],
+    colors = ['rgb(208,209,230)', 'rgb(103,169,207)', 'rgb(1,108,89)'],
+    radii = [4, 8, 12];
+
+map.on('click', 'median_income', (event) => {
+    new mapboxgl.Popup()
+        .setLngLat(event.features[0].geometry.coordinates)
+        .setHTML(`<strong>Median Family Income ($):</strong> ${event.features[0].properties.MedianFamilyIncome}`)
+        .addTo(map);
+});
+
+// create legend object, it will anchor to the div element with the id legend.
+const legend = document.getElementById('legend');
+
+//set up legend grades and labels
+var labels = ['<strong>Median Family Income ($):</strong>'],
+    vbreak;
+//iterate through grades and create a scaled circle and label for each
+for (var i = 0; i < grades.length; i++) {
+    vbreak = grades[i];
+    // you need to manually adjust the radius of each dot on the legend 
+    // in order to make sure the legend can be properly referred to the dot on the map.
+    dot_radius = 2 * radii[i];
+    labels.push(
+        '<p class="break"><i class="dot" style="background:' + colors[i] + '; width: ' + dot_radius +
+        'px; height: ' +
+        dot_radius + 'px; "></i> <span class="dot-label" style="top: ' + dot_radius / 2 + 'px;">' + vbreak +
+        '</span></p>');
+
+}
+
+/*const source =
+    '<p style="text-align: right; font-size:10pt">Source: <a href="https://github.com/nytimes/covid-19-data/blob/43d32dde2f87bd4dafbb7d23f5d9e878124018b8/live/us-counties.csv">NYTimes</a></p>'; */
+
+// combine all the html codes.
+legend.innerHTML = labels.join('') + source;
 
 
 
