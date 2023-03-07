774f213648714c94628291b3e2ac2160d7407e66
Mitchell S <m.e.stapelman@gmail.com>
2023-03-06 19:51:12 -0800

    remove script, formatting

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,8 +2,8 @@
 const navbar = `
     <nav class="navbar navbar-expand-lg>
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">Food Deserts in Washington</a>
-            <a class="btn btn-light me-2" href="about.html" role="button">About Us</a>
+            <a class="navbar-brand" href="index.html">Washington Food Accessibility</a>
+            <a class="btn btn-light me-2" href="about.html" role="button"">About Us</a>
         </div>
     </nav>
     `
