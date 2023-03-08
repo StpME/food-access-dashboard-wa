ebbebedb1075d3572ad01e2b0d4d6444daecf61f
Mitchell S <m.e.stapelman@gmail.com>
2023-03-07 17:16:47 -0800

    readd zoom function

--- scripts/mapbox.js
+++ scripts/mapbox.js
@@ -187,6 +187,14 @@ function showCensusTractStats(e) {
         return;
     }
     var feature = features[0];
+    // Zoom to tract on-click
+    let zoomCoord = [feature.properties.INTPTLON, feature.properties.INTPTLAT,];
+         map.flyTo({
+             center: zoomCoord,
+             zoom: 8,
+             essential: true // this animation is considered essential with respect to prefers-reduced-motion
+             });
+
     // If tract is invalid/missing key props, reset to default values
     if (feature.properties.Pop2010 === undefined || feature.properties.Pop2010 === null) {
         infoPanel.innerHTML = initialContent
@@ -202,16 +210,11 @@ function showCensusTractStats(e) {
     }
 }
 // Casts value to str & adds a comma every 3 letter then returns the new str. 
+map.on('click', 'a', showCensusTractStats);
+
 function formatter(value) {
     str_value = String(value);
     var regex = /(\d)(?=(\d{3})+$)/g;
     return str_value.replace(regex, '$1,');
 } 
-map.on('click', 'a', showCensusTractStats);
-
-// let zoomCoord = [feature.properties.INTPTLON, feature.properties.INTPTLAT,];
-//         map.flyTo({
-//             center: zoomCoord,
-//             zoom: 8,
-//             essential: true // this animation is considered essential with respect to prefers-reduced-motion
-//             });
+ 
